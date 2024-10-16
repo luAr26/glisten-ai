f5f15de65d3924555378f9decb29a56639dc13ed
Raul <raul_cosmin_26@yahoo.com>
2024-10-16 19:53:56 +0300

    Initial commit.

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/Navbar";
+import { createClient } from "@/prismicio";
+
+const Header = async () => {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+  return (
+    <header>
+      <Navbar settings={settings} />
+    </header>
+  );
+};
+
+export default Header;
